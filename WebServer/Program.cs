a28e42c24afcaab1b7a98157c080d39b6775af40
Sebastiano Merlino <etr@zencoders.org>
2011-04-03 14:46:30 +0000

    Http and Tcp ports are now configurable!

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -5,6 +5,7 @@ using TransportService;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Configuration;
 
 namespace ConsoleHost
 {
@@ -12,36 +13,42 @@ namespace ConsoleHost
     {
         static void Main(string[] args)
         {
-            try
+            if (args.Length < 1 || (args.Length >= 1 && args[0] != "configuration"))
             {
+                string httpPort = WCFServiceHost.Properties.Settings.Default.httpPort;
+                string tcpPort = WCFServiceHost.Properties.Settings.Default.tcpPort;
+                Uri[] addresses = new Uri[2];
+                addresses[0] = new Uri("http://localhost:" + httpPort + "/TransportProtocol/");
+                addresses[1] = new Uri("net.tcp://localhost:" + tcpPort + "/TrasportProtocol/");
                 Type serviceType = typeof(TransportProtocol);
-                ServiceHost host = new ServiceHost(serviceType);
-
-                host.Open();
-                #region Output dispatchers listening
-                foreach (Uri uri in host.BaseAddresses)
+                ServiceHost host = new ServiceHost(serviceType, addresses);
+                try
                 {
-                    Console.WriteLine("\t{0}", uri.ToString());
-                }
+                    host.Open();
+                    #region Output dispatchers listening
+                    foreach (Uri uri in host.BaseAddresses)
+                    {
+                        Console.WriteLine("\t{0}", uri.ToString());
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine("Number of dispatchers listening : {0}", host.ChannelDispatchers.Count);
+                    foreach (System.ServiceModel.Dispatcher.ChannelDispatcher dispatcher in host.ChannelDispatchers)
+                    {
+                        Console.WriteLine("\t{0}, {1}", dispatcher.Listener.Uri.ToString(), dispatcher.BindingName);
+                    }
 
-                Console.WriteLine();
-                Console.WriteLine("Number of dispatchers listening : {0}", host.ChannelDispatchers.Count);
-                foreach (System.ServiceModel.Dispatcher.ChannelDispatcher dispatcher in host.ChannelDispatchers)
+                    Console.WriteLine();
+                    Console.WriteLine("Press <ENTER> to terminate Host");
+                    Console.ReadLine();
+                    #endregion
+                }
+                catch (AddressAlreadyInUseException)
                 {
-                    Console.WriteLine("\t{0}, {1}", dispatcher.Listener.Uri.ToString(), dispatcher.BindingName);
+                    Console.WriteLine("Unable to Connect like a Server because there is already one on this machine");
                 }
-
-                Console.WriteLine();
-                Console.WriteLine("Press <ENTER> to terminate Host");
-                Console.ReadLine();
-                #endregion
-            }
-            catch (AddressAlreadyInUseException)
-            {
-                Console.WriteLine("Unable to Connect like a Server because there is already one on this machine");
             }
             //Example CODE
-            if(args.Length >= 1) {
+            if(args.Length >= 1 && args[0] != "configuration") {
 //                ChunkRequest chkrq = new ChunkRequest(System.Convert.ToInt32(args[0]), args[1], System.Convert.ToInt32(args[2]));
                 Transferer tsf = new Transferer();
                 Dictionary<string, float> p2p = new Dictionary<string,float>();
@@ -60,8 +67,14 @@ namespace ConsoleHost
                 }
 //                Console.WriteLine("Remote Serving Buffer is: {0}", result.ServingBuffer);
             }
-            //
+            else if (args.Length >= 1 && args[0] == "configuration")
+            {
+                Console.WriteLine("New Http Port:");
+                WCFServiceHost.Properties.Settings.Default.httpPort = Console.ReadLine();
+                Console.WriteLine("New Tcp Port:");
+                WCFServiceHost.Properties.Settings.Default.tcpPort = Console.ReadLine();
+                WCFServiceHost.Properties.Settings.Default.Save();
+            }
         }
     }
 }
-
