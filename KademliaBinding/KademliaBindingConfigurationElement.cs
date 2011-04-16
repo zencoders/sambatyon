c3a54d6f325305c0489a383afff238735f275c82
Sebastiano Merlino <etr@zencoders.org>
2011-04-16 15:07:39 +0000

    Added Kademlia Binding to the project; it will be used to wrap kademlia inside a binding

---
+++ KademliaBinding/KademliaBindingConfigurationElement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel.Configuration;
+
+namespace KademliaBinding
+{
+    public class KademliaBindingConfigurationElement : StandardBindingElement
+    {
+        public KademliaBindingConfigurationElement(string configurationName)
+            : base(configurationName)
+        {
+        }
+
+        public KademliaBindingConfigurationElement()
+            : this(null)
+        {
+        }
+
+        protected override Type BindingElementType
+        {
+            get { return typeof(NetKademliaBinding); }
+        }
+    }
+}
---
+++ KademliaBinding/NetKademliaBinding.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace KademliaBinding
+{
+    public class NetKademliaBinding : Binding
+    {
+        public NetKademliaBinding()
+        {
+            Initialize();
+        }
+        void Initialize()
+        {
+ /*           transport = new UdpTransportBindingElement();
+            session = new ReliableSessionBindingElement();
+            compositeDuplex = new CompositeDuplexBindingElement();
+            encoding = new TextMessageEncodingBindingElement();*/
+        }
+    }
+}
