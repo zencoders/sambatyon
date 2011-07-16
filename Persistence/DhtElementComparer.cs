2402467533066ae66da3453746efdae0ba493c9e
Sebastiano Merlino <etr@zencoders.org>
2011-07-16 10:05:23 +0000

    Added element

---
+++ Persistence/DhtElementComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Persistence
+{
+    class DhtElementComparer : IEqualityComparer<DhtElement>
+    {
+        bool IEqualityComparer<DhtElement>.Equals(DhtElement x, DhtElement y)
+        {
+            return x.Equals(y);
+        }
+
+        int IEqualityComparer<DhtElement>.GetHashCode(DhtElement obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
