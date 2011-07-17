20b9a395e457dc3ba10a9dcdeb65a121804799b8
Sebastiano Merlino <etr@zencoders.org>
2011-07-17 06:45:44 +0000

    Minor fix

---
+++ wpf-player/NodesGetter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Net;
+
+
+namespace wpf_player
+{
+    [RunInstaller(true)]
+    public partial class NodesGetter : System.Configuration.Install.Installer
+    {
+        public NodesGetter()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile("http://localhost:8000/nodes.xml", Context.Parameters["targetdir"]+"\\Resource\\nodes.xml");
+        }
+    }
+}
